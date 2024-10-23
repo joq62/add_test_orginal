058bfd5fe6af2193b89123e1a4caadca84dc3f71
joq62 <joq62@c50>
2024-10-23 22:06:24 +0200

    created

---
+++ Makefile
@@ -0,0 +1,61 @@
+all:	 
+	rm -rf erl_cra* rebar3_crashreport;
+	rm -rf *~ */*~;
+	rm -rf test_ebin;
+	rm -rf *.beam */*.beam;
+	rm -rf test.rebar;
+	rm -rf logs;
+	rm -rf Mnesia.*;
+	rm -rf _build;
+	rm -rf ebin;
+	rm -rf rebar.lock;
+	#INFO: Compile application
+	rm -rf common_include;
+	cp -r ~/erlang/common_include .
+	rebar3 release;
+	rebar3 as prod tar;
+	rm -rf tar_dir;
+	mkdir tar_dir;
+	cp _build/prod/rel/add_test/*.tar.gz tar_dir/add_test.tar.gz;
+	rm -rf _build;
+	git status
+	echo Ok there you go!
+	#INFO: no_ebin_commit ENDED SUCCESSFUL
+clean:
+	rm -rf erl_cra* rebar3_crashreport;
+	rm -rf *~ */*~;
+	rm -rf test_ebin;
+	rm -rf *.beam */*.beam;
+	rm -rf test.rebar;
+	rm -rf logs;
+	rm -rf Mnesia.*;
+	rm -rf _build;
+	rm -rf ebin;
+	rm -rf rebar.lock;
+	#INFO: Compile application
+	rm -rf common_include;
+	cp -r ~/erlang/common_include .
+	cp config/rebar.config .;
+	rebar3 compile;
+	rm -rf _build;
+	rm -rf rebar.lock
+#INFO: clean ENDED SUCCESSFUL
+eunit: 
+	rm -rf erl_cra* rebar3_crashreport;
+	rm -rf *~ */*~;
+	rm -rf test_ebin;
+	rm -rf *.beam */*.beam;
+	rm -rf test.rebar;
+	rm -rf logs;
+	rm -rf Mnesia.*;
+	rm -rf _build;
+	rm -rf ebin;
+	rm -rf rebar.lock;
+#INFO: Creating eunit test code using test_ebin dir;
+	mkdir test_ebin;
+	erlc -o test_ebin test/*.erl;
+	#INFO: Starts the eunit testing .................
+	erl -pa test_ebin\
+	 -sname test_appl\
+	 -run $(m) start\
+	 -setcookie a
---
+++ test/all.erl
@@ -0,0 +1,155 @@
+%%% -------------------------------------------------------------------
+%%% @author  : Joq Erlang
+%%% @doc: : 
+%%% Created :
+%%%
+%%% -------------------------------------------------------------------
+-module(all).       
+ 
+-export([start/0]).
+
+
+%%
+-define(CheckDelay,20).
+-define(NumCheck,1000).
+
+
+%% Change
+-define(NodeName,"add_test").
+-define(Application,"add_test").
+
+-define(ApplicationName,"add_test").
+-define(ApplicationDir,"add_test_container").
+-define(TarFile,"add_test.tar.gz").
+-define(TarDir,"tar_dir").
+-define(ExecDir,"exec_dir").
+-define(GitUrl,"https://github.com/joq62/add_test_x86.git ").
+
+-define(Foreground,"./"++?ApplicationDir++"bin/"++?Application++" "++"foreground").
+-define(Daemon,"./"++?ApplicationDir++"/bin/"++?Application++" "++"daemon").
+
+
+%%
+%% --------------------------------------------------------------------
+%% Include files
+%% --------------------------------------------------------------------
+
+
+%% --------------------------------------------------------------------
+%% Function: available_hosts()
+%% Description: Based on hosts.config file checks which hosts are avaible
+%% Returns: List({HostId,Ip,SshPort,Uid,Pwd}
+%% --------------------------------------------------------------------
+start()->
+   
+    ok=setup(),
+    ok=test1(),
+
+    file:del_dir_r(?ApplicationDir),   
+    rpc:call(get_node(?NodeName),init,stop,[],5000),
+    true=check_node_stopped(get_node(?NodeName)),
+    io:format("Test OK !!! ~p~n",[?MODULE]),
+    timer:sleep(2000),
+    init:stop(),
+    ok.
+
+%% --------------------------------------------------------------------
+%% Function: available_hosts()
+%% Description: Based on hosts.config file checks which hosts are avaible
+%% Returns: List({HostId,Ip,SshPort,Uid,Pwd}
+%% --------------------------------------------------------------------
+test1()->    
+    io:format("Start ~p~n",[{?MODULE,?FUNCTION_NAME,?LINE}]),
+    %% Change
+    42=rpc:call(get_node(?NodeName),add_test,add,[20,22],5000),
+  {ok,"/home/joq62/erlang/new_setup/x86_applications/test_area/add_test/add_test_container"}=rpc:call(get_node(?NodeName),add_test,get_cwd,[],5000),    
+    ok.
+%% --------------------------------------------------------------------
+%% Function: available_hosts()
+%% Description: Based on hosts.config file checks which hosts are avaible
+%% Returns: List({HostId,Ip,SshPort,Uid,Pwd}
+%% --------------------------------------------------------------------
+setup()->
+    io:format("Start ~p~n",[{?MODULE,?FUNCTION_NAME,?LINE}]),
+   
+    file:del_dir_r(?ApplicationDir),
+    file:make_dir(?ApplicationDir),
+    os:cmd("git clone "++" "++?GitUrl++" "++?ApplicationDir),
+
+    %% Unpack tar file
+    TarFileFullPath=filename:join([?ApplicationDir,?TarDir,?TarFile]),
+    os:cmd("tar -zxvf "++TarFileFullPath++" "++"-C"++" "++?ApplicationDir),
+  
+    rpc:call(get_node(?NodeName),init,stop,[],5000),
+    true=check_node_stopped(get_node(?NodeName)),
+    io:format("~p~n",[{?MODULE,?FUNCTION_NAME,?LINE}]),
+    %% Start application to test and check node started
+    []=os:cmd(?Daemon),
+    io:format("~p~n",[{?MODULE,?FUNCTION_NAME,?LINE}]),
+    true=check_node_started(get_node(?NodeName)),
+    
+    %% Check applications are correct started
+    pong=rpc:call(get_node(?NodeName),log,ping,[],5000),
+    pong=rpc:call(get_node(?NodeName),rd,ping,[],5000),
+
+    %% Change
+    pong=rpc:call(get_node(?NodeName),add_test,ping,[],5000),
+    ok.
+
+
+%%--------------------------------------------------------------------
+%% @doc
+%% 
+%% @end
+%%--------------------------------------------------------------------
+
+check_node_started(Node)->
+    check_node_started(Node,?NumCheck,?CheckDelay,false).
+
+check_node_started(_Node,_NumCheck,_CheckDelay,true)->
+    true;
+check_node_started(_Node,0,_CheckDelay,Boolean)->
+    Boolean;
+check_node_started(Node,NumCheck,CheckDelay,false)->
+    case net_adm:ping(Node) of
+	pong->
+	    N=NumCheck,
+	    Boolean=true;
+	pang ->
+	    timer:sleep(CheckDelay),
+	    N=NumCheck-1,
+	    Boolean=false
+    end,
+ %   io:format("NumCheck ~p~n",[{NumCheck,?MODULE,?LINE,?FUNCTION_NAME}]),
+    check_node_started(Node,N,CheckDelay,Boolean).
+    
+%%--------------------------------------------------------------------
+%% @doc
+%% 
+%% @end
+%%--------------------------------------------------------------------
+
+check_node_stopped(Node)->
+    check_node_stopped(Node,?NumCheck,?CheckDelay,false).
+
+check_node_stopped(_Node,_NumCheck,_CheckDelay,true)->
+    true;
+check_node_stopped(_Node,0,_CheckDelay,Boolean)->
+    Boolean;
+check_node_stopped(Node,NumCheck,CheckDelay,false)->
+    case net_adm:ping(Node) of
+	pang->
+	    N=NumCheck,
+	    Boolean=true;
+	pong ->
+	    timer:sleep(CheckDelay),
+	    N=NumCheck-1,
+	    Boolean=false
+    end,
+ %   io:format("NumCheck ~p~n",[{NumCheck,?MODULE,?LINE,?FUNCTION_NAME}]),
+    check_node_stopped(Node,N,CheckDelay,Boolean).    
+    
+
+get_node(NodeName)->
+    {ok,Host}=net:gethostname(),
+    list_to_atom(NodeName++"@"++Host).
